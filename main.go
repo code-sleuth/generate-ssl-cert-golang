502c5cb646d3cb4ac500d570734848926912cdfb
Ibrahim Mbaziira <code.ibra@gmail.com>
2019-03-21 17:05:58 +0300

    generate ssl cert

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net"
+	"net/http"
+	"time"
+	"os"
+)
+
+func main(){
+	generateSslCert()
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+		Handler: nil,
+	}
+	server.ListenAndServeTLS("cert.pem", "key.pem")
+}
+
+func generateSslCert(){
+	max := new(big.Int)
+	serialNumber := max.Exp(big.NewInt(2), big.NewInt(130), nil).Sub(max, big.NewInt(1))
+	subject := pkix.Name{
+		Organization: []string{"Ibrahim Extreme Engeneering"},
+		OrganizationalUnit: []string{"Engineering"},
+		CommonName: "Software Engineering",
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: subject,
+		NotBefore: time.Now(),
+		NotAfter: time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		IPAddresses: []net.IP{net.ParseIP("127.0.0.1")},
+	}
+
+	pk, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	derBytes, _ := x509.CreateCertificate(rand.Reader, &template, &template, &pk.PublicKey, pk)
+
+	certOut, _ := os.Create("cert.pem")
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	certOut.Close()
+
+	keyOut, _ := os.Create("key.pem")
+	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(pk)})
+	keyOut.Close()
+}
